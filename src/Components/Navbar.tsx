06d5773ec5b5bb4e6700b32138f3d0b064a072fb
SmokedMonkey420 <ciankarlo09@gmail.com>
2024-05-20 14:37:19 +0800

    Some changes

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(flase);
   return (
     <>
       <nav className="navbar">
@@ -14,6 +15,37 @@ function Navbar() {
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Destinations
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Members
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Gallery
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
