39b7b4ad21c8f801558f4afcf752561ffb1adce4
SmokedMonkey420 <ciankarlo09@gmail.com>
2024-05-22 01:16:47 +0800

    Finished Suggestion Cards Move to Match Cards

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -18,13 +18,11 @@ function Cards() {
             <CardItem
               src="images/image2.jpg"
               text="Explore the baguio with various tourist spots and adventures waiting"
-              label="Adventure"
               path="/services"
             />
             <CardItem
               src="images/img-2.jpg"
               text="Travel through the Islands of Bali in a Private Cruise"
-              label="Luxury"
               path="/services"
             />
           </ul>
@@ -32,19 +30,16 @@ function Cards() {
             <CardItem
               src="images/img-3.jpg"
               text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-              label="Mystery"
               path="/services"
             />
             <CardItem
               src="images/img-4.jpg"
               text="Experience Football on Top of the Himilayan Mountains"
-              label="Adventure"
               path="/products"
             />
             <CardItem
               src="images/img-8.jpg"
               text="Ride through the Sahara Desert on a guided camel tour"
-              label="Adrenaline"
               path="/sign-up"
             />
           </ul>
--- src/Components/MatchCard.tsx
+++ src/Components/MatchCard.tsx
@@ -4,31 +4,32 @@ import SuggestionCardItem from "./SuggestionCardItem";
 
 function MatchCard() {
   return (
-    <div className="cards">
+    <div className="result__cards">
       <div className="match__card__container">
         <div className="match__card__wrapper">
           <ul className="match__card__items"></ul>
         </div>
       </div>
+      <h1>Here are our other recommended spots!</h1>
       <div className="suggestion__card__container">
         <div className="suggestion__card__wrapper">
           <ul className="suggestion__card__items">
             <SuggestionCardItem
               src="/images/image2.jpg"
               name="Baguio City"
-              description="Lorem ipsum"
+              description="Baguio, on the Philippines’ Luzon island, is a mountain town of universities and resorts. Called the “City of Pines,” it’s particularly popular in summer due to unusually cooler weather."
               path="/baguio-city"
             />
             <SuggestionCardItem
               src="/images/image2.jpg"
               name="Baguio City"
-              description="Lorem ipsum"
+              description="Baguio, on the Philippines’ Luzon island, is a mountain town of universities and resorts. Called the “City of Pines,” it’s particularly popular in summer due to unusually cooler weather."
               path="/baguio-city"
             />
             <SuggestionCardItem
               src="/images/image2.jpg"
               name="Baguio City"
-              description="Lorem ipsum"
+              description="Baguio, on the Philippines’ Luzon island, is a mountain town of universities and resorts. Called the “City of Pines,” it’s particularly popular in summer due to unusually cooler weather."
               path="/baguio-city"
             />
           </ul>
