811fabf2ff0811f2012c0f4d12e14bb2bce02065
SmokedMonkey420 <ciankarlo09@gmail.com>
2024-11-06 16:54:20 +0800

    Configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://smokedmonkey420.github.io/Travel-Site/",
   plugins: [react()],
 })
